8f281b41f512103079944f5e36edc1d14f2e7397
Mikaelbr073 <mcb3@discente.ifpe.edu.br>
2023-08-23 11:37:15 -0300

    first commit

--- README.md
+++ README.md
@@ -1,38 +1,32 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
-## Getting Started
+# Landing page for hotel with NEXT.js 🏢
+The project involves the creation of a modern and captivating landing page for a "Hotel" using the Next.js and Tailwind CSS technologies. The hotel's concept is to provide a serene and inspiring environment, centered around learning and productivity. Through the synergy of advanced technologies and elegant design, the page aims to captivate visitors and convey the hotel's unique atmosphere.
 
-First, run the development server:
+
+
+## Stack⛏
+
+**Front-end:** Next.js, TailwindCSS
+
+
+
+
+## Screenshots
+
+![App Screenshot](https://via.placeholder.com/468x300?text=App+Screenshot+Here)
+
+![App Screenshot](https://via.placeholder.com/468x300?text=App+Screenshot+Here)
+
+
+## Install and running project
+
+Install my-project with npm
 
 ```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
+  npm install
 ```
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `pages/index.tsx`. The page auto-updates as you edit the file.
-
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.ts`.
-
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+## Running Project
+```bash
+  npm run dev
+```
+    
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,232 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import style from '../styles/backgroud.module.css'
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='h-screen bg-gray-50 text-gray-600'>
+     
+       <div>
+        {/* Header */}
+        <div className='container mx-auto max-w-5xl px-4 h-24 flex items-center justify-between'>
+          <div className='font-black text-gray-400'>
+            Logo
+          </div>
+          <div className='hidden md:block'>
+            <ul className='flex space-x-4'>
+              <li>
+                <a className='text-sm text-gray-600 hover:text-gray-800 hover:underline' href="">Como Reservar</a>
+              </li>
+              <li>
+                <a className='text-sm text-gray-600 hover:text-gray-800 hover:underline' href="">Sobre Nós</a>
+              </li>
+              <li>
+                <a className='text-sm text-gray-600 hover:text-gray-800 hover:underline' href="">Informações</a>
+              </li>
+              <li>
+                <a className='text-sm text-gray-600 hover:text-gray-800 hover:underline' href="">Contato</a>
+              </li>
+            </ul>
+          </div>
+          <div className='md:hidden'>
+            <button className='text-gray-600 hover:text-gray-800'>
+              <svg className='h-6 w-6 fill-current' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+                <path d='M4 6h16M4 12h16M4 18h16'></path>
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/*header/*/}
+
+      {/*bannner*/}
+      <div className={style.imagemBackground}>
+        <div className='container mx-auto max-w-5xl px-4 h-full flex items-center justify-center'>
+          <div>
+            <div className='text-center'>
+              <h1 className='text-gray-100 text-3xl'>Onde Você quer ir?</h1>
+              <p className='text-green-100'>Escola o estado e cidade para achar os melhores resorts.</p>
+            </div>
+            <div>
+
+            <div className='mt-7'>
+              <form className='grid grid-cols-2 gap-4 md:grid-cols-3'>
+                <select className='bg-gray-50 px-2 py-2 border-gray-300 rounded text-gray-500'>
+                  <option value="">Escolha o Estado</option>
+                </select>
+                <select  className='bg-gray-50 px-2 py-2 border-gray-300 rounded text-gray-500'>
+                  <option value="">Escolha a Cidadde</option>
+                </select>
+                <button className='inline-flex items-center col-span-2 lg:col-span-1 text-yellow-900 bg-yellow-500 font-medium px-3 py-2 rounded'>
+                <svg className='h-4 w-4 mr-2 ' fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"></path>
+                </svg>  
+                Pesquisar</button>
+              </form>
+            </div>
+
+            </div>
+          </div>
+        </div>
       </div>
+      {/*bannner/*/}
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {/*featured resort*/}
+      <div>
+        <div className='container max-w-5xl mx-auto px-4 py-10'>
+          <div className='flex flex-col-reverse lg:flex-row'>
+            <div className='lg:pr-10'>
+              <div className='text-sm text-gray-400 font-medium'>RESORT EM DESTAQUE</div>
+              <div className='text-2xl'> Ecoresort Parai do forte Bhaia</div>
+              <div className='text-sm text-gray-400 font-medium mt-2'>Hotel qualidade 5 estrelas</div>
+              <div>
+                <p className='text-sm text-gray-400 font-medium mt-4 mb-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis blanditiis, voluptatem molestias qui, consequuntur, harum nostrum recusandae cumque adipisci sunt quos ad. Quibusdam recusandae repellat voluptatum, cumque nemo ducimus.</p>
+                <p className='text-sm text-gray-400 font-medium mt-4'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis blanditiis, voluptatem molestias qui, consequuntur, harum nostrum recusandae cumque adipisci sunt quos ad. Quibusdam recusandae repellat voluptatum, cumque nemo ducimus.</p>
+    
+              </div>
+              <div className='mt-4'>
+                <button  className='inline-flex items-center mt-3 text-yellow-900 bg-yellow-500 font-medium px-3 py-2 rounded'>
+                  Solicitar Reserva
+                  <svg  className='w-4 h-4 ml-2' fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"></path>
+                  </svg>
+                  </button>
+              </div>
+            </div>
+            <div className=' flex-shrink-0 lg:ml-auto mb-5 lg:mb-0 '>
+              <Image
+              src='/images/featured-img.png'
+              width={453}
+              height={268}
+              alt='teste'
+              />
+            </div>
+          </div>
+        </div>
       </div>
-    </main>
-  )
+      {/*featured resort/*/}
+
+      {/*resort grid*/}
+      <div className="container max-w-5xl mx-auto px-4">
+        <div>
+          <h3 className='text-2xl text-center text-gray-500 font-semibold mb-8'>
+          Conheça mais resorts</h3>
+        </div>
+        <div className='md:grid md:grid-cols-4 md:grid-rows-2 md:gap-6 space-y-4 md:space-y-0'>
+
+          <div className='relative md:col-span-2 row-span-2 h-100'>
+            <div className='absolute bottom-0 ml-4 mb-4 text-white font-semibold z-20'>
+              Beach park resort
+            </div>
+            <Image
+                className='absolute bottom-0 left-0 right-0 h-20 z-10'
+                src='/images/image-shadow.png'
+                width={800}
+                height={460}
+                alt='teste'
+                />
+            <Image
+                className='w-full h-full object-cover rounded-lg overflow-hidden '
+                src='/images/hotel-1.jpeg'
+                width={800}
+                height={460}
+                alt='teste'
+                />
+          </div>
+
+          <div className=' relative md:col-span-2 h-60'>
+          <div className='absolute bottom-0 ml-4 mb-4 text-white font-semibold z-20'>
+              Salinas do Maragogi
+            </div>
+            <Image
+                className='absolute bottom-0 left-0 right-0 h-20 z-10'
+                src='/images/image-shadow.png'
+                width={800}
+                height={460}
+                alt='teste'
+                />
+            <Image
+                className='w-full h-full object-cover rounded-lg overflow-hidden'
+                src='/images/hotel-2.jpeg'
+                width={1024}
+                height={575}
+                alt='teste'
+                />
+          </div>
+
+          <div className='h-50 relative'>
+          <div className='absolute bottom-0 ml-4 mb-4 text-white font-semibold z-20'>
+              Grand Pallandium
+            </div>
+            <Image
+                className='absolute bottom-0 left-0 right-0 h-20 z-10'
+                src='/images/image-shadow.png'
+                width={980}
+                height={560}
+                alt='teste'
+                />
+            <Image
+                className='w-full h-full object-cover rounded-lg overflow-hidden'
+                src='/images/hotel-3.jpeg'
+                width={1280}
+                height={720}
+                alt='teste'
+                />
+          </div>
+
+          <div className='relative'>
+          <div className='absolute bottom-0 ml-4 mb-4 text-white font-semibold z-20'>
+              Arraial d'ajuda Eco Resort
+            </div>
+            <Image
+                className='absolute bottom-0 left-0 right-0 h-20 z-10'
+                src='/images/image-shadow.png'
+                width={800}
+                height={460}
+                alt='teste'
+                />
+            <Image
+                className='w-full h-full object-cover rounded-lg overflow-hidden'
+                src='/images/hotel-4.jpeg'
+                width={900}
+                height={0}
+                alt='teste'
+                />
+          </div>
+
+        </div>
+      </div> 
+      {/*resort grid/*/}
+
+      {/*form resort*/}
+      <div>
+        <div className='container max-w-5xl mx-auto px-4 py-20 flex justify-center'>
+        <form className='grid grid-cols-2 gap-4 md:grid-cols-3'>
+                <select className='bg-gray-50 px-2 py-2 border-gray-300 rounded text-gray-500'>
+                  <option value="">Escolha o Estado</option>
+                </select>
+                <select  className='bg-gray-50 px-2 py-2 border-gray-300 rounded text-gray-500'>
+                  <option value="">Escolha a Cidadde</option>
+                </select>
+                <button className='inline-flex items-center col-span-2 lg:col-span-1 text-yellow-900 bg-yellow-500 font-medium px-3 py-2 rounded'>
+                <svg className='h-4 w-4 mr-2 ' fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"></path>
+                </svg>  
+                Pesquisar</button>
+          </form>
+        </div>
+      </div>
+      {/*form resort/*/}
+
+      {/*footer*/}
+      <div className="border-t border-gray-200">
+        <div className='container  max-w-5xl mx-auto px-4 flex mt-4 h-40 items-center justify-between'>
+          <div className='font-black text-gray-400'>
+            Logo
+          </div>
+        </div>
+      </div>
+      {/*footer/*/}
+    </div>
+  );
 }
